a69b8ae883d450afe4f5902e5a36d827c71cc91b
dyang46 <103404171+dyang46@users.noreply.github.com>
2023-12-05 01:09:53 -0800

    Step 2 Done

--- src/main.ts
+++ src/main.ts
@@ -10,3 +10,26 @@ const header = document.createElement("h1");
 header.innerHTML = gameName;
 app.append(header);
 
+//set click counter 
+let clickCount = 0;
+
+function trackButtonClick() {
+  clickCount++;
+  updateClickCount();
+}
+//print #click
+function updateClickCount() {
+  const clickCountElement = document.getElementById('clickCount');
+  if (clickCountElement) {
+    clickCountElement.textContent = clickCount.toString();
+  }
+}
+//track each click
+const button = document.getElementById('trackButton');
+if (button) {
+  button.addEventListener('click', trackButtonClick);
+}
+
+
+
+
