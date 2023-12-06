719b8d0311782d16011f4aad3e31579a7afaf70b
dyang46 <103404171+dyang46@users.noreply.github.com>
2023-12-05 18:00:36 -0800

    Step 4 Completed

--- src/main.ts
+++ src/main.ts
@@ -8,13 +8,18 @@ document.title = gameName;
 
 const header = document.createElement("h1");
 header.innerHTML = gameName;
+
 const Butest = document.createElement("button");
 Butest.innerHTML = "🕷️";
+
 const unitName = document.createElement("div");
-unitName.id = "a";
+
 unitName.innerHTML= "Nest ";
+
 const numDislay = document.createElement("div");
-numDislay.id = "lemme";
+numDislay.id = "num";
+
+app.append(header,Butest,unitName, numDislay);
 
 let clickCount = 0;
 
@@ -23,21 +28,33 @@ function trackButtonClick() {
   console.log(clickCount);
   updateClickCount();
 }
+
 function updateClickCount() {
-  const clickCountElement = document.getElementById("lemme");
+  const clickCountElement = document.getElementById("num");
   if (clickCountElement) {
     clickCountElement.textContent = clickCount.toString();
   }
 }
+
 //track each click
 
 Butest.addEventListener('click', trackButtonClick);
 
-app.append(header,Butest,unitName, numDislay);
+//Old Step 3 code
 
-
-setInterval(() => {
+/*setInterval(() => {
   clickCount++;
   updateClickCount();
-}, 1000);
-
+}, 1000);*/
+let k = 0;
+function movediv(timestamp){
+  //if (timestamp % 100 ==0){
+    k++;
+    if (k % 60 == 0){clickCount++;
+    updateClickCount();}
+  //}
+  //clickCount++;
+  
+  requestAnimationFrame(movediv);
+}
+requestAnimationFrame(movediv);
